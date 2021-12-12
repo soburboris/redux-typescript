b93f2b0ba35553bca54d11a52776c52fcac17456
Boris Sobur <59290755+soburboris@users.noreply.github.com>
2021-12-12 20:46:51 +0600

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,17 +9,44 @@ interface AppProps {
 	deleteTodo: typeof deleteTodo
 }
 
-class _App extends React.Component<AppProps> {
+interface AppState {
+	fetching: boolean
+}
+
+
+class _App extends React.Component<AppProps, AppState> {
+
+	constructor (props:AppProps) {
+		super(props)
+		this.state = {fetching: false}
+	}
+
+	
+	componentDidUpdate(prevProps: AppProps):void {
+		if (!prevProps.todos.length && this.props.todos.length){
+			this.setState({fetching: false})
+	}
+	
+}
 
 	onButtonClick = ():void => {
-		this.props.fetchTodos()
+	
+			this.props.fetchTodos()
+		this.setState({fetching: true})
+		
+		setTimeout(() => {
+this.setState({fetching: false})
+		},500)
+		
+	
+		
 	}
 
 	onTodoClick = (id:number): void => {
 		this.props.deleteTodo(id)
 	}
 
-	listTodos (item:Todo): JSX.Element{
+	renderTodos (item:Todo): JSX.Element{
 		return(
 			<li onClick={ () => this.onTodoClick(item.id)} key ={item.id}>{item.title}</li>
 		)
@@ -31,9 +58,10 @@ class _App extends React.Component<AppProps> {
 		 return (
 			 <div>
 				 <button onClick ={this.onButtonClick}>Fetch</button>
-				 {todos && 
+				 {this.state.fetching?<h3>Loading</h3> :null}
+				 {todos && !this.state.fetching &&
 				 <div>
-					 <ul>{todos.map((item) => this.listTodos(item))}</ul>
+					 <ul>{todos.map((item) => this.renderTodos(item))}</ul>
 				 </div> }
 			 </div>
 		 )
