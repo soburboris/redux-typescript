10b407283fc57002b8cfa42664ac6003dfddb12a
Boris Sobur <59290755+soburboris@users.noreply.github.com>
2021-12-12 14:13:13 +0600

    create

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from '@testing-library/react'
+import React from 'react'
+import  ReactDOM  from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+interface AppProps {
+	color?: string
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// interface AppState {
+// 	counter: number
+// }
+
+
+
+class App extends React.Component<AppProps> {
+
+	state = { counter: 0}
+
+	// constructor(props: AppProps) {
+	// 	super(props)
+
+	// 	this.state = {counter: 0}
+	// }
+
+	
+
+	onInc ():void  {
+		this.setState({counter: this.state.counter + 1})
+
+	}
+
+	onDec ():void {
+		this.setState({counter: this.state.counter - 1})
+	}
+
+
+	render() {
+			return (
+				<div>
+					<button onClick={this.onInc.bind(this)}>+</button>
+					<button onClick={this.onDec.bind(this)}>-</button>
+					<span>{this.state.counter}</span>
+				</div>)
+	}
+}
+
+ReactDOM.render(<App color ='white'/>, document.querySelector('#root'))
